bfd39e8d18f26b0fe6dcb2aba8cad2ad21de8876
Russ Berg <russ.berg@lightsource.ca>
2021-12-22 09:04:29 -0600

    cleanup

--- PIE712App/PIE712WithXpsAsyn/go.bat
+++
@@ -1,5 +0,0 @@
-make
-cd O.win32-x86-debug
-mt.exe /manifest uhvAbsMotor.exe.manifest "/outputresource:uhvAbsMotor.exe;#1"
-cd ..
-make
--- goPIE712Motors.bat
+++
@@ -1,7 +0,0 @@
-set IOCDIR=C:/controls/epics/R3.14.12.4/modules/support/motor-6-8/iocBoot/iocPIE712WithAsyn/
-
-cd C:\controls\epics\R3.14.12.4\modules\support\motor-6-8\
-
- .\bin\win32-x86-debug\pi_e712WithAsyn.exe .\iocBoot\iocPIE712WithAsyn\st.cmd.pi_e712
-# .\bin\win32-x86-debug\pi_e712WithAsyn.exe 
-
